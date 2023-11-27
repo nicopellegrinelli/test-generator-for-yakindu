cfdf5ff37a93217b0546b5f0d1dad2002c61ce64
PellegrinelliNico <n.pellegrinelli@studenti.unibg.it>
2023-11-27 16:29:24 +0100

    small updates

--- ysc-to-sctunit-translator/JUnit2SCTUnit/src/main/java/junitreading/TestCollector.java
+++ ysc-to-sctunit-translator/JUnit2SCTUnit/src/main/java/junitreading/TestCollector.java
@@ -1,8 +1,11 @@
 package junitreading;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
@@ -15,27 +18,19 @@ public class TestCollector extends VoidVisitorAdapter<List<TestCase>> {
 	@Override
 	public void visit(MethodDeclaration md, List<TestCase> collector) {
 		super.visit(md, collector);
-
-		if (md.getBody().toString().contains(".enter")) {
+		
+		if (md.getBody().toString().contains(".enter") && !md.getBody().toString().contains(".setIsExecuting")) {
 
 			List<VariableDeclarationExpr> variableDeclarationList = new ArrayList<VariableDeclarationExpr>();
 			VoidVisitor<List<VariableDeclarationExpr>> variableDeclarationListExprCollector = new VariableDeclarationExprCollector();
 			variableDeclarationListExprCollector.visit(md, variableDeclarationList);
-
-			for (VariableDeclarationExpr fieldAccess : variableDeclarationList) {
-				System.out.println(fieldAccess.getChildNodes().toString());
+			
+			Map<String, String> variables = new HashMap<String, String>();
+			
+			for (VariableDeclarationExpr variableDecl : variableDeclarationList) {
+				List<Node> n = variableDecl.getChildNodes().get(0).getChildNodes();
+				variables.put(n.get(1).toString(), n.get(2).toString());
 			}
-
-//			String state = "";
-//			for (FieldAccessExpr fieldAccess : fieldAccessList) {
-//				System.out.println(fieldAccess.toString());
-//				if (!fieldAccess.getChildNodes().get(0).toString().contains(".State"))
-//					state += fieldAccess.getChildNodes().get(0).toString();
-//				else
-//					state += "." + fieldAccess.getChildNodes().get(1).toString().toLowerCase().replace("_", ".");
-//				if (state.contains("final"))
-//					state = state.replace(".final.", "_final_");
-//			}
 
 			List<MethodCallExpr> methodCallList = new ArrayList<MethodCallExpr>();
 			VoidVisitor<List<MethodCallExpr>> methodCallExprCollector = new MethodCallExprCollector();
@@ -58,16 +53,27 @@ public class TestCollector extends VoidVisitorAdapter<List<TestCase>> {
 				}
 				if (methodName.equals("assertTrue") || methodName.equals("assertFalse")) {
 					boolean assertTrue = methodName.equals("assertTrue");
-					String parameter = methodCall.getArgument(0).toString();
-					if (parameter.contains(".isActive()")) {
+					String parameterName = methodCall.getArgument(0).toString();
+					String parameterValue = variables.get(parameterName);
+					if (parameterName.contains(".isActive()")  
+							|| (parameterValue != null && parameterValue.contains(".isActive()"))) {
 						testCase.addIsActive(assertTrue);
 						continue;
 					}
-					if (parameter.contains(".isFinal()")) {
+					if (parameterName.contains(".isFinal()")
+							|| (parameterValue != null && parameterValue.contains(".isFinal()"))) {
 						testCase.addIsFinal(assertTrue);
 						continue;
 					}
-					// testCase.addAssertState(state, assertTrue);
+					if(parameterValue != null && parameterValue.contains(".isStateActive")) {
+						String stateName = variables.get(parameterValue.substring(parameterValue.indexOf('(')+1, parameterValue.indexOf(')')));
+						stateName = stateName.replace("State.", "");
+						int separator = stateName.indexOf('.');
+						stateName = stateName.substring(0, separator) + stateName.substring(separator).toLowerCase().replace("_", ".");
+						if (stateName.contains("final"))
+							stateName = stateName.replace(".final.", "_final_");
+						testCase.addAssertState(stateName, assertTrue);
+					}
 				}
 			}
 
