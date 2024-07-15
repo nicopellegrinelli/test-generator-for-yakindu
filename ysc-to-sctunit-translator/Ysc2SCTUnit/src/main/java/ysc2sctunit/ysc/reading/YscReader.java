f5325bf8abe4b83aba9cc2e9f505a096804cfc63
PellegrinelliNico <n.pellegrinelli@studenti.unibg.it>
2024-07-15 09:12:11 +0200

    Upload library files

--- ysc-to-sctunit-translator/Ysc2SCTUnit/src/main/java/ysc2sctunit/ysc/reading/YscReader.java
+++ ysc-to-sctunit-translator/Ysc2SCTUnit/src/main/java/ysc2sctunit/ysc/reading/YscReader.java
@@ -18,7 +18,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class YscReader, contains the information of a statechart read from a ysc
  * file.
