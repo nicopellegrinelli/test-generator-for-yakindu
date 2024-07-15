61edad599d43c694a1c5c7a662533ac934957dc0
PellegrinelliNico <67684836+PellegrinelliNico@users.noreply.github.com>
2024-07-15 08:18:45 +0200

    Delete sctunit-gen.bat

--- sctunit-gen.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set projectPath=ProgettiGit\test-generator-for-yakindu\itemis-ws\Test
-set dir=models\from_other_repos
-
-cd C:\Users\lenovo\Desktop
-
-for /f %%f in ('dir /b .\%projectPath%\%dir%\*.ysc ^| sort') do java -jar .\ysc2sctunit-with-default-0.0.2.jar -scc .\itemis_CREATE\scc.bat -projectPath .\%projectPath% -sourceDir %dir% -sourceFile %%~nf -targetPackage statechart -evoTestDir test
